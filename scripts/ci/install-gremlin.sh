5660950758d55502a7ff210385e4ab2df898ad06
Nicolas PLANEL <nplanel@redhat.com>
2016-04-21 15:51:45 +1000

    [CI] functional tests : update gremlin server zip file
    Use another source as apache.crihan.fr return 404
    
    Change-Id: I4f3c8e50077c0b842b275cf876cd25d610c3c4b4

--- scripts/ci/install-gremlin.sh
+++ scripts/ci/install-gremlin.sh
@@ -4,6 +4,6 @@ set -v
 
 # Install apache gremlin server
 cd ${HOME}
-curl -s -L http://apache.crihan.fr/dist/incubator/tinkerpop/3.1.1-incubating/apache-gremlin-server-3.1.1-incubating-bin.zip > gremlin-server.zip
+curl -s -L http://archive.apache.org/dist/incubator/tinkerpop/3.1.1-incubating/apache-gremlin-server-3.1.1-incubating-bin.zip > gremlin-server.zip
 unzip gremlin-server.zip
 export GREMLINPATH=${HOME}/apache-gremlin-server-3.1.1-incubating
