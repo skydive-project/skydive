f4191c3490974e77a8015ff7bb4e5dd658978b98
Nicolas PLANEL <nplanel@redhat.com>
2016-04-21 21:53:14 +1000

    [CI] unzip update
    Launch unzip with -u update flag, useful in interactive shell.
    
    Change-Id: I68e4a91ff0cbecdafca47219539f50dc94227e2a

--- scripts/ci/install-gremlin.sh
+++ scripts/ci/install-gremlin.sh
@@ -5,5 +5,5 @@ set -v
 # Install apache gremlin server
 cd ${HOME}
 curl -s -L http://archive.apache.org/dist/incubator/tinkerpop/3.1.1-incubating/apache-gremlin-server-3.1.1-incubating-bin.zip > gremlin-server.zip
-unzip gremlin-server.zip
+unzip -u gremlin-server.zip
 export GREMLINPATH=${HOME}/apache-gremlin-server-3.1.1-incubating
