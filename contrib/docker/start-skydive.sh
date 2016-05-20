ba06e75c5c699cf97c0373a2ac00708cb67f3e6b
Sylvain Afchain <safchain@redhat.com>
2016-05-20 09:54:53 +0200

    contrib: add kubernetes yaml file
    This yaml file allows to deploy Skydive
    within a kubernetes cluster starting
    an analyzer and multiple agents in
    hostNetwork mode.
    
    Change-Id: Ie9ac0a7a6bd419ebab320f5375f601339c3f7e84

--- contrib/docker/start-skydive.sh
+++ contrib/docker/start-skydive.sh
@@ -3,6 +3,9 @@
 ELASTICSEARCH_PORT="${ELASTICSEARCH_PORT:-127.0.0.1:9200}"
 SKYDIVE_ANALYZER_PORT="${ANALYZER_PORT:-127.0.0.1:8082}"
 SKYDIVE_ANALYZER_PORT_2379_ADDR="${ANALYZER_PORT_2379_ADDR:-127.0.0.1:2379}"
+OVSDB="${OVSDB:-/var/run/openvswitch/db.sock}"
+SKYDIVE_NETNS_RUN_PATH="${SKYDIVE_NETNS_RUN_PATH:-/host/run}"
+SKYDIVE_LOG_LEVEL="${SKYDIVE_LOG_LEVEL:-INFO}"
 
 if [ ! -e /etc/skydive.yml ]
 then
@@ -10,6 +13,17 @@ then
 agent:
   listen: 0.0.0.0:8081
   analyzers: $SKYDIVE_ANALYZER_PORT
+  topology:
+    probes:
+      - netlink
+      - netns
+      - ovsdb
+      - docker
+      - neutron
+  flow:
+    probes:
+      - ovssflow
+      - pcap
 analyzer:
   listen: 0.0.0.0:8082
 etcd:
@@ -18,6 +32,14 @@ etcd:
     - http://$SKYDIVE_ANALYZER_PORT_2379_ADDR
 storage:
   elasticsearch: $ELASTICSEARCH_PORT
+ovs:
+  ovsdb: $OVSDB
+docker:
+  url: unix:///var/run/docker.sock
+netns:
+  run_path: $SKYDIVE_NETNS_RUN_PATH
+logging:
+  default: $SKYDIVE_LOG_LEVEL
 EOF
 fi
 
