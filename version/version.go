92c8c07ccd7e3a2de09fe3bd9165c8846a960e57
Sylvain Afchain <safchain@redhat.com>
2016-08-04 11:43:20 +0200

    Bump skydive version to 0.4.0
    Change-Id: Id123392dfe7b58b38268dfbe518d875fb17a01ac

--- version/version.go
+++ version/version.go
@@ -14,7 +14,7 @@ var Package = "github.com/skydive-project/skydive"
 // the latest release tag by hand, always suffixed by "+unknown". During
 // build, it will be replaced by the actual version. The value here will be
 // used if Skydive is run after a go get based install.
-var Version = "v0.3.0+unknown"
+var Version = "v0.4.0+unknown"
 
 // FprintVersion outputs the version string to the writer, in the following
 // format, followed by a newline:
