abba09767d72f9d8d14f95afa08f7ec9f4b5b51f
Sylvain Baubeau <sbaubeau@redhat.com>
2016-10-10 19:12:28 +0200

    Set default analyzer address for client
    Change-Id: I7c4f6ce8e33ec6b04a24ee3bbf745705ed46a53d
    Reviewed-on: http://softwarefactory-project.io/r/5049
    Reviewed-by: Sylvain Afchain <safchain@gmail.com>
    Tested-by: Sylvain Afchain <safchain@gmail.com>
    Workflow: Sylvain Afchain <safchain@gmail.com>
    Tested-by: Jenkins CI <jenkins@softwarefactory-project.io>

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -27,6 +27,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/skydive-project/skydive/config"
 	shttp "github.com/skydive-project/skydive/http"
 	"github.com/skydive-project/skydive/logging"
 	"github.com/spf13/cobra"
@@ -35,6 +36,7 @@ import (
 var (
 	authenticationOpts shttp.AuthenticationOpts
 	gremlinQuery       string
+	analyzerAddr       string
 )
 
 var Client = &cobra.Command{
@@ -42,6 +44,13 @@ var Client = &cobra.Command{
 	Short:        "Skydive client",
 	Long:         "Skydive client",
 	SilenceUsage: true,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if flag := cmd.Flags().Lookup("analyzer"); !flag.Changed {
+			config.GetConfig().Set("agent.analyzers", "localhost:8082")
+		} else {
+			config.GetConfig().Set("agent.analyzers", flag.Value)
+		}
+	},
 }
 
 func printJSON(obj interface{}) {
@@ -62,6 +71,7 @@ func setFromFlag(cmd *cobra.Command, flag string, value *string) {
 func init() {
 	Client.PersistentFlags().StringVarP(&authenticationOpts.Username, "username", "", os.Getenv("SKYDIVE_USERNAME"), "username auth parameter")
 	Client.PersistentFlags().StringVarP(&authenticationOpts.Password, "password", "", os.Getenv("SKYDIVE_PASSWORD"), "password auth parameter")
+	Client.PersistentFlags().String("analyzer", "", "analyzer address")
 
 	Client.AddCommand(AlertCmd)
 	Client.AddCommand(CaptureCmd)
