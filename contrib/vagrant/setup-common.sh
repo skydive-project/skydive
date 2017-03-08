1e35d95bafe6ca37e101da2f45f24d119589fde3
Sylvain Afchain <safchain@redhat.com>
2017-03-08 16:45:27 +0100

    travis: re-add binary build for up-to-date vagrant skydive
    Need to have an up-to-date skydive version corresponding
    to the skydive config file of vagrant.
    
    Change-Id: Ia924847d137c51462c9529a2053fc886ba4c8b0b
    Reviewed-on: https://softwarefactory-project.io/r/6861
    Reviewed-by: Sylvain Baubeau <sbaubeau@redhat.com>
    Tested-by: Sylvain Baubeau <sbaubeau@redhat.com>
    Workflow: Sylvain Baubeau <sbaubeau@redhat.com>

--- contrib/vagrant/setup-common.sh
+++ contrib/vagrant/setup-common.sh
@@ -3,5 +3,5 @@
 sudo dnf -y install nfs-utils nfs-utils-lib jq
 sudo setenforce 0
 sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
-sudo curl -o /usr/bin/skydive -L `curl -s https://api.github.com/repos/skydive-project/skydive/releases/latest | jq --raw-output '.assets[0] | .browser_download_url'`
+sudo curl -o /usr/bin/skydive -L https://github.com/skydive-project/skydive-binaries/raw/travis-builds/skydive-latest
 sudo chmod +x /usr/bin/skydive
