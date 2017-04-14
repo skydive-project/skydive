1017f77aceebe845aefd1fe7a25c959c2bdce3f2
Sylvain Baubeau <sbaubeau@redhat.com>
2017-04-14 11:27:35 +0200

    flow: enable gopacket and pcapsocket probes by default
    Change-Id: Id5c540e94c74e253548bc99be13aabbcaf1745e3
    Reviewed-on: https://softwarefactory-project.io/r/7501
    Reviewed-by: Sylvain Afchain <safchain@gmail.com>
    Tested-by: Sylvain Afchain <safchain@gmail.com>
    Workflow: Sylvain Afchain <safchain@gmail.com>
    Tested-by: Jenkins CI <jenkins@softwarefactory-project.io>

--- config/config.go
+++ config/config.go
@@ -83,6 +83,7 @@ func init() {
 	cfg.SetDefault("openstack.endpoint_type", "public")
 	cfg.SetDefault("agent.topology.probes", []string{"netlink", "netns", "ovsdb"})
 	cfg.SetDefault("agent.topology.netlink.metrics_update", 30)
+	cfg.SetDefault("agent.flow.probes", []string{"gopacket", "pcapsocket"})
 	cfg.SetDefault("agent.flow.pcapsocket.bind_address", "127.0.0.1")
 	cfg.SetDefault("agent.flow.pcapsocket.min_port", 8100)
 	cfg.SetDefault("agent.flow.pcapsocket.max_port", 8132)
