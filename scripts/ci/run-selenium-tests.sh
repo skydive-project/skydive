6ddfa260a6f6cea19825f2cfbac87f90c0299dfd
Sylvain Baubeau <sbaubeau@redhat.com>
2017-07-07 15:33:30 +0200

    selenium: allow recording video with conversion to gif

--- scripts/ci/run-selenium-tests.sh
+++ scripts/ci/run-selenium-tests.sh
@@ -17,4 +17,12 @@ make install
 export SKYDIVE_ANALYZERS=localhost:8082
 export SKYDIVE=${GOPATH}/bin/skydive
 
+if [ "$RECORD" = "true" ]; then
+  ARGS="$ARGS -record"
+fi
+
 make test.functionals TAGS="selenium test" VERBOSE=true TIMEOUT=10m TEST_PATTERN=Selenium ARGS="$ARGS -standalone"
+
+for i in `ls tests/*.mp4 | tail -n 1`; do
+  "${dir}/convert-to-gif.sh" $i 0
+done
