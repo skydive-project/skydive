ae2d576c21b28352c2b8ea6cfab389e98a1a160b
Nicolas PLANEL <nplanel@redhat.com>
2017-08-30 20:32:37 +1000

    govendor : use github gist to speed up govendor sync

--- scripts/ci/install-go.sh
+++ scripts/ci/install-go.sh
@@ -28,15 +28,6 @@ go get -f -u github.com/tebeka/go2xunit
 export PATH=$PATH:$GOPATH/bin
 
 # speedup govendor sync command
-curl -o /tmp/vendor.tgz http://46.231.132.68:8080/v1/AUTH_0ec9e4f4f3044236b4d18536ccfcb182/skydive/vendor/vendor.tar.gz
-
-pushd ${GOPATH}/src/github.com/skydive-project/skydive
-go get -f -u github.com/kardianos/govendor
-govendor sync -n | perl -pe 's|fetch \"(.*)\"$|vendor/\1|g' | sort -u > vendor.fetch.list
-cat vendor.fetch.list | xargs tar -xvzf /tmp/vendor.tgz --exclude "vendor/vendor.json"
-# remove installed
-find vendor/ -mindepth 2 -type f | xargs dirname | sort -u > vendor.installed.list
-echo "package to be removed/cleanup"
-diff -u vendor.fetch.list vendor.installed.list | grep '^\+v' | perl -pe 's|^\+(.*)|\1|' | tee /dev/stdout | xargs -n 1 rm -rf
-rm -f vendor.fetch.list vendor.installed.list
-popd
+mkdir -p $HOME/.govendor $GOPATH/.cache
+rm -rf $GOPATH/.cache/govendor
+ln -s $HOME/.govendor $GOPATH/.cache/govendor
