cd7146e3431be9e5e9ba75d284bd6a93233f1f12
Sylvain Baubeau <sbaubeau@redhat.com>
2017-09-01 10:13:48 +0200

    vagrant: use ansible playbooks instead of shell scripts

--- contrib/vagrant/setup-common.sh
+++ contrib/vagrant/setup-common.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo dnf -y install nfs-utils nfs-utils-lib jq
+sudo dnf -y install python
 sudo setenforce 0
 sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
-sudo curl -o /usr/bin/skydive -L https://github.com/skydive-project/skydive-binaries/raw/travis-builds/skydive-latest
-sudo chmod +x /usr/bin/skydive
