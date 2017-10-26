b236d2cf18d92026e0d82e0756d0c8118509354a
Sylvain Baubeau <sbaubeau@redhat.com>
2017-10-26 18:25:58 +0200

    api: allow the config api to return arrays and maps

--- api/config.go
+++ api/config.go
@@ -16,14 +16,32 @@ type configAPI struct {
 	cfg *viper.Viper
 }
 
+func normalizeValue(v interface{}) interface{} {
+	switch v := v.(type) {
+	case map[interface{}]interface{}:
+		m := make(map[string]interface{}, len(v))
+		for key, value := range v {
+			key := key.(string)
+			value = normalizeValue(value)
+			m[key] = value
+		}
+		return m
+	case []interface{}:
+		for i, value := range v {
+			v[i] = normalizeValue(value)
+		}
+	}
+	return v
+}
+
 func (c *configAPI) configGet(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
 	vars := mux.Vars(&r.Request)
-	// lookup into configAPI
-	Value := c.cfg.GetString(vars["key"])
+	key := vars["key"]
+	value := normalizeValue(c.cfg.Get(key))
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(Value); err != nil {
+	if err := json.NewEncoder(w).Encode(value); err != nil {
 		panic(err)
 	}
 }
