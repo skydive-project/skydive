e3518971c25939e34a37fe85d0214e80cf7f38f1
Sylvain Afchain <safchain@gmail.com>
2017-11-13 13:04:01 +0100

    ui: add value transformer to object-details to improve readability

--- statics/js/components/object-detail.js
+++ statics/js/components/object-detail.js
@@ -13,8 +13,15 @@ Vue.component('object-detail', {
 
     links: {
       type: Object
-    }
+    },
 
+    transformer: {
+      type: Function
+    },
+
+    path: {
+      type: String
+    }
   },
 
   template: '\
@@ -28,28 +35,35 @@ Vue.component('object-detail', {
             <div v-for="(v, index) in value">\
               <div v-if="typeof v == \'object\'" class="object-sub-detail" style="margin-left: 20px;">\
                 <span class="object-key" :class="typeof(value)" style="float:left">- </span>\
-                <object-detail :object="v"></object-detail>\
+                <object-detail :object="v" :path="path ? path+\'.\'+key : key" :transformer="transformer"></object-detail>\
               </div>\
               <div v-else class="object-sub-detail">\
-                <div class="object-detail" :class="typeof(value)">- {{v}}</div>\
+                <div class="object-detail" :class="typeof(value)">- {{ transform(path ? path+\'.\'+key : key, v) }}</div>\
               </div>\
             </div>\
           </div>\
         </div>\
         <div v-else-if="typeof value == \'object\'" class="object-sub-detail">\
           <span class="object-key">{{key}}</span>\
-          <object-detail :object="value"></object-detail>\
+          <object-detail :object="value" :path="path ? path+\'.\'+key : key" :transformer="transformer"></object-detail>\
         </div>\
         <div v-else>\
           <span class="object-key">{{key}}</span> :\
-          <span class="object-value" :class="typeof(value)">{{value}}</span>\
-          <i v-if="links && links[key]" class="indicator glyphicon glyphicon-download-alt raw-packet-link" @click="links[key]"></i>\
+          <span class="object-value" :class="typeof(value)" v-html="transform(path ? path+\'.\'+key : key, value)"></span>\
+          <i v-if="links && links[key]" :class="links[key].class" @click="links[key].fnc"></i>\
         </div>\
       </div>\
     </div>\
   ',
 
   methods: {
+
+    transform: function(key, value) {
+      if (this.transformer) {
+        return this.transformer(key, value);
+      }
+      return value;
+    },
 
     collapsedByDefault: function(key) {
       if (key === "FDB" || key === "Neighbors") return true;
