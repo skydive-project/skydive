cec365762dce258123dddb0220a250a7ba8adfd0
Aidan Shribman <aidan.shribman@gmail.com>
2017-12-18 11:49:25 +0000

    ui: improve setup of fixed images

--- statics/js/components/layout.js
+++ statics/js/components/layout.js
@@ -1,38 +1,38 @@
-var hostImg = 'statics/img/host.png';
-var switchImg = 'statics/img/switch.png';
-var portImg = 'statics/img/port.png';
-var intfImg = 'statics/img/intf.png';
-var vethImg = 'statics/img/veth.png';
-var nsImg = 'statics/img/ns.png';
-var bridgeImg = 'statics/img/bridge.png';
-var dockerImg = 'statics/img/docker.png';
-var podImg = 'statics/img/pod.png';
-var networkPolicyImg = 'statics/img/networkpolicy.png';
-var neutronImg = 'statics/img/openstack.png';
-var k8sImg = 'statics/img/k8s.png';
-
-var minusImg = 'statics/img/minus-outline-16.png';
-var plusImg = 'statics/img/plus-16.png';
-var captureIndicatorImg = 'statics/img/media-record.png';
-var pinIndicatorImg = 'statics/img/pin.png';
-
-var nodeImgMap = {
-  "host": hostImg,
-  "port": portImg,
-  "ovsport": portImg,
-  "bridge": bridgeImg,
-  "switch": switchImg,
-  "ovsbridge": switchImg,
-  "netns": nsImg,
-  "veth": vethImg,
-  "bond": portImg,
-  "container": dockerImg,
-  "pod": podImg,
-  "networkpolicy": networkPolicyImg
+var getImagePath = function(label) {
+	return 'statics/img/'+label+'.png';
 };
 
-var managerImgMap = {
-  "docker": dockerImg,
-  "neutron": neutronImg,
-  "k8s": k8sImg
+var minusImg = getImagePath('minus-outline-16');
+var plusImg = getImagePath('plus-16');
+var captureIndicatorImg = getImagePath('media-record');
+var pinIndicatorImg = getImagePath('pin');
+
+var setupFixedImages = function(labelMap) {
+  imgMap = {};
+  Object.keys(labelMap).forEach(function(key) {
+    imgMap[key] = getImagePath(labelMap[key]);
+  });
+  return imgMap;
 };
+
+var nodeImgMap = setupFixedImages({
+  "host": "host",
+  "port": "port",
+  "ovsport": "port",
+  "bridge": "bridge",
+  "switch": "switch",
+  "ovsbridge": "switch",
+  "netns": "ns",
+  "veth": "veth",
+  "bond": "port",
+  "container": "docker",
+  "pod": "pod",
+  "networkpolicy": "networkpolicy",
+  "default": "intf",
+});
+
+var managerImgMap = setupFixedImages({
+  "docker": "docker",
+  "neutron": "openstack",
+  "k8s": "k8s",
+});
