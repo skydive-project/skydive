4a4b1aecea9199a6044f5f5c86511d590bc273cc
Aidan Shribman <aidan.shribman@gmail.com>
2018-05-09 09:13:19 +0000

    minikube: replace tap with spaces

--- scripts/ci/install-minikube.sh
+++ scripts/ci/install-minikube.sh
@@ -17,106 +17,106 @@ export CHANGE_MINIKUBE_NONE_USER=true
 export KUBECONFIG=$HOME/.kube/config
 
 uninstall_binary() {
-	local prog=$1
-	sudo rm -f $TARGET_DIR/$prog
+        local prog=$1
+        sudo rm -f $TARGET_DIR/$prog
 }
 
 install_binary() {
-	local prog=$1
-	local url=$2
+        local prog=$1
+        local url=$2
 
-	wget --no-check-certificate -O $prog $url
-	if [ $? != 0 ]; then
-		echo "failed to download $url"
-		exit 1
-	fi
+        wget --no-check-certificate -O $prog $url
+        if [ $? != 0 ]; then
+                echo "failed to download $url"
+                exit 1
+        fi
 
-	chmod 0777 $prog
-	sudo mv $prog $TARGET_DIR/$prog
+        chmod 0777 $prog
+        sudo mv $prog $TARGET_DIR/$prog
 }
 
 check_minikube() {
-	which minikube 2>/dev/null
-	if [ $? != 0 ]; then
-		echo "minikube is not installed. Please run install-minikube.sh install"
-		exit 1
-	fi
+        which minikube 2>/dev/null
+        if [ $? != 0 ]; then
+                echo "minikube is not installed. Please run install-minikube.sh install"
+                exit 1
+        fi
 
-	sudo systemctl start docker
+        sudo systemctl start docker
 }
 
 install() {
-	install_binary minikube $MINIKUBE_URL
-	install_binary kubectl $KUBECTL_URL
+        install_binary minikube $MINIKUBE_URL
+        install_binary kubectl $KUBECTL_URL
 }
 
 uninstall() {
-	uninstall_binary minikube
-	uninstall_binary kubectl
+        uninstall_binary minikube
+        uninstall_binary kubectl
 }
 
 stop() {
-	check_minikube
+        check_minikube
 
-	sudo -E minikube delete
-	sudo rm -rf $HOME/.minikube $HOME/.kube
-	sudo rm -rf /root/.minikube /root/.kube
+        sudo -E minikube delete
+        sudo rm -rf $HOME/.minikube $HOME/.kube
+        sudo rm -rf /root/.minikube /root/.kube
 
-	sudo docker system prune -af
-	for i in $(sudo docker ps -aq --filter name=k8s); do
-		sudo docker stop $i
-		sudo docker rm $i
-	done
+        sudo docker system prune -af
+        for i in $(sudo docker ps -aq --filter name=k8s); do
+                sudo docker stop $i
+                sudo docker rm $i
+        done
 
-	sudo systemctl stop localkube
-	sudo systemctl disable localkube
+        sudo systemctl stop localkube
+        sudo systemctl disable localkube
 }
 
 start() {
-	check_minikube
+        check_minikube
 
-	local args="--vm-driver=none"
-	local driver=$(sudo docker info --format '{{print .CgroupDriver}}')
-	if [ -n "$driver" ]; then
-		args="$args --extra-config=kubelet.CgroupDriver=$driver"
-	fi
+        local args="--vm-driver=none"
+        local driver=$(sudo docker info --format '{{print .CgroupDriver}}')
+        if [ -n "$driver" ]; then
+                args="$args --extra-config=kubelet.CgroupDriver=$driver"
+        fi
 
-       	sudo -E minikube start $args
-	sudo -E minikube addons disable dashboard
-	minikube status
-	kubectl config use-context minikube
+               sudo -E minikube start $args
+        sudo -E minikube addons disable dashboard
+        minikube status
+        kubectl config use-context minikube
 
-	# enable accesss from root account
-	for i in .kube .minikube; do
-		sudo rm -rf /root/$i
-		sudo cp -ar $HOME/$i /root/$i
-	done
+        # enable accesss from root account
+        for i in .kube .minikube; do
+                sudo rm -rf /root/$i
+                sudo cp -ar $HOME/$i /root/$i
+        done
 }
 
 status() {
-	kubectl version
-	kubectl config get-contexts
-	minikube status
+        kubectl version
+        kubectl config get-contexts
+        minikube status
 }
 
 case "$1" in
-	install)
-		install
-		;;
-	uninstall)
-		uninstall
-		;;
-	start)
-		start
-		;;
-	stop)
-		stop
-		;;
-	status)
-		status
-		;;
-	*)
-		echo "$0 [install|uninstall|start|stop|status]"
-		exit 1
-		;;
+        install)
+                install
+                ;;
+        uninstall)
+                uninstall
+                ;;
+        start)
+                start
+                ;;
+        stop)
+                stop
+                ;;
+        status)
+                status
+                ;;
+        *)
+                echo "$0 [install|uninstall|start|stop|status]"
+                exit 1
+                ;;
 esac
