fc2f9cee14c1e55983541baeb043872ae08a34d7
Antoine Eiche <lewo@abesis.fr>
2018-07-18 18:32:42 +0200

    opencontrail: add routing table in Contrail metadatas
    This commit adds the Contrail routing table in the Contrail metadata
    of a node. It relies on the Contrail binary `rt` to be notified on
    route updates.
    
    The routing table is added in the Contrail metadata and not in already
    existing routing table attributes because it seems hard to maintain
    the routing table with two producers: the netlink probe squashes the
    whole routing table metadata on each route updates.

--- tests/opencontrail_test.go
+++ tests/opencontrail_test.go
@@ -40,3 +40,33 @@ func TestOpenContrailTopology(t *testing.T) {
 
 	RunTest(t, test)
 }
+
+func TestOpenContrailRoutingTable(t *testing.T) {
+	test := &Test{
+		setupCmds: []helper.Cmd{
+			{"contrail-create-network.py default-domain:default-project:vn1", true},
+			{"netns-daemon-start -n default-domain:default-project:vn1 vm1", true},
+		},
+		tearDownCmds: []helper.Cmd{
+			// We should delete the net
+			{"netns-daemon-stop vm1", true},
+		},
+
+		checks: []CheckFunction{func(c *CheckContext) error {
+			gh := c.gh
+
+			gremlin := g.G.V().Has("Contrail.RoutingTable.Prefix", "20.1.1.253/32")
+			nodes, err := gh.GetNodes(gremlin)
+			if err != nil {
+				return err
+			}
+
+			if len(nodes) != 1 {
+				return fmt.Errorf("Expected 1 node, got %+v", nodes)
+			}
+			return nil
+		}},
+	}
+
+	RunTest(t, test)
+}
