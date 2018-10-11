8c44c840a164f3320a1090f4ea839bffaeb3b268
Sylvain Afchain <safchain@gmail.com>
2018-10-11 14:43:52 +0200

    agent: warn if agent doesn't run as root

--- topology/probes/netns/netns.go
+++ topology/probes/netns/netns.go
@@ -324,10 +324,6 @@ func (u *Probe) Exclude(paths ...string) {
 
 // NewProbe creates a new network namespace probe
 func NewProbe(g *graph.Graph, n *graph.Node, nlProbe *netlink.Probe) (*Probe, error) {
-	if uid := os.Geteuid(); uid != 0 {
-		logging.GetLogger().Warning("NetNS probe needs root permissions, network namespace might not be repported")
-	}
-
 	ns, err := netns.Get()
 	if err != nil {
 		return nil, errors.New("Failed to get root namespace")
