d795a4c0f02b69f4dd623cf231e7f7aa451be502
Sergey Glazyrin <s.glazyrin@partner.samsung.com>
2018-11-19 14:31:03 +0100

    added sriov probe
    responsibilites are:
    1. take care of realtime changes in vf configuration
    2. take care of realtime change for physical devices which provide sriov function
    3. add all vfs currently configured on system
    4. update state of the physical interface which provide sriov function
    Partial-Bug: #SOFTID-200
    
    Change-Id: I7736e052460ec83489c7360722d22e345f2d1960
    Signed-off-by: Sergey Glazyrin <s.glazyrin@partner.samsung.com>

--- agent/probes.go
+++ agent/probes.go
@@ -40,6 +40,7 @@ import (
 	"github.com/skydive-project/skydive/topology/probes/opencontrail"
 	"github.com/skydive-project/skydive/topology/probes/ovsdb"
 	"github.com/skydive-project/skydive/topology/probes/socketinfo"
+	"github.com/skydive-project/skydive/topology/probes/sriov"
 )
 
 // NewTopologyProbeBundleFromConfig creates a new topology probe.Bundle based on the configuration
@@ -114,6 +115,12 @@ func NewTopologyProbeBundleFromConfig(g *graph.Graph, hostNode *graph.Node) (*pr
 				return nil, fmt.Errorf("Failed to initialize Libvirt probe: %s", err)
 			}
 			probes[t] = libvirt
+		case "sriov":
+			sriov, err := sriov.NewProbe(g, hostNode)
+			if err != nil {
+				return nil, fmt.Errorf("Failed to initialize Sriov probe: %s", err)
+			}
+			probes[t] = sriov
 		default:
 			logging.GetLogger().Errorf("unknown probe type %s", t)
 		}
