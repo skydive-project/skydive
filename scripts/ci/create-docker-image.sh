5433856937e4802349dee899f78f30507b72bf3e
Aidan Shribman <aidan.shribman@gmail.com>
2019-01-09 08:34:43 +0000

    ci: create docker image supporting SKIP_BUILD, SKIP_PUSH, SKIP_MANIFEST

--- scripts/ci/create-docker-image.sh
+++ scripts/ci/create-docker-image.sh
@@ -21,68 +21,81 @@ if [ "${DOCKER_IMAGE%/*/*}" != "${DOCKER_IMAGE}" ]; then
     DOCKER_SERVER=${DOCKER_IMAGE%/*/*}
 fi
 
-if [ -n "$PUSH_RUN" ]; then
-    echo "Running in push run mode. Skipping build."
-else
+docker_build() {
     for arch in $ARCHES
     do
+        flags="DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_TAG=${arch}-${DOCKER_TAG}"
         case $arch in
           amd64)
             # x86_64 image
-            make docker-build DOCKER_IMAGE=${DOCKER_IMAGE} DOCKER_TAG=amd64-${DOCKER_TAG} ${BASE:+BASE=$BASE}
+            make docker-build ${flags} ${BASE:+BASE=$BASE}
             ;;
           ppc64le)
-            make docker-cross-build WITH_EBPF=true TARGET_ARCH=powerpc64le TARGET_GOARCH=ppc64le DEBARCH=ppc64el DOCKER_IMAGE=${DOCKER_IMAGE} BASE=${BASE:-${arch}/ubuntu} DOCKER_TAG=$arch-${DOCKER_TAG}
+            make docker-cross-build ${flags} WITH_EBPF=true TARGET_ARCH=powerpc64le TARGET_GOARCH=$arch DEBARCH=ppc64el BASE=${BASE:-${arch}/ubuntu}
             ;;
           arm64)
-            make docker-cross-build WITH_EBPF=true TARGET_ARCH=aarch64 TARGET_GOARCH=arm64 DOCKER_IMAGE=${DOCKER_IMAGE} BASE=${BASE:-aarch64/ubuntu} DOCKER_TAG=$arch-${DOCKER_TAG}
+            make docker-cross-build ${flags} WITH_EBPF=true TARGET_ARCH=aarch64 TARGET_GOARCH=$arch BASE=${BASE:-aarch64/ubuntu}
             ;;
           s390x)
-            make docker-cross-build WITH_EBPF=true TARGET_ARCH=s390x TARGET_GOARCH=s390x DOCKER_IMAGE=${DOCKER_IMAGE} BASE=${BASE:-${arch}/ubuntu} DOCKER_TAG=$arch-${DOCKER_TAG}
+            make docker-cross-build ${flags} WITH_EBPF=true TARGET_ARCH=$arch TARGET_GOARCH=$arch BASE=${BASE:-${arch}/ubuntu}
             ;;
           *)
-            make docker-cross-build WITH_EBPF=true TARGET_ARCH=$arch TARGET_GOARCH=$arch DOCKER_IMAGE=${DOCKER_IMAGE} BASE=${BASE:-${arch}/ubuntu} DOCKER_TAG=$arch-${DOCKER_TAG}
+            make docker-cross-build ${flags} WITH_EBPF=true TARGET_ARCH=$arch TARGET_GOARCH=$arch BASE=${BASE:-${arch}/ubuntu}
             ;;
         esac
     done
-fi
+}
+
+docker_login() {
+    set +x
+    if [ -z "$DOCKER_PASSWORD" ]; then
+        echo "The environment variable DOCKER_PASSWORD needs to be defined"
+        exit 1
+    fi
+
+    echo "${DOCKER_PASSWORD}" | docker login  --username "${DOCKER_USERNAME}" --password-stdin ${DOCKER_SERVER}
+    set -x
+}
+
+docker_push() {
+    for arch in $ARCHES
+    do
+        docker push ${DOCKER_IMAGE}:${arch}-${DOCKER_TAG}
+    done
+}
+
+docker_manifest() {
+    digests=""
+    for arch in $ARCHES
+    do
+        digest=$( docker inspect --format='{{index .RepoDigests 0}}' ${DOCKER_IMAGE}:${arch}-${DOCKER_TAG} )
+        digests="${digests} $digest"
+    done
+
+    res=0
+    for i in {1..6}
+    do
+        docker manifest create --amend "${DOCKER_IMAGE}:${DOCKER_TAG}" ${digests} && break || res=$?
+        sleep 10
+    done
+    [ $res != 0 ] && exit $res
+
+    for arch in $ARCHES
+    do
+        digest=$( docker inspect --format='{{index .RepoDigests 0}}' ${DOCKER_IMAGE}:${arch}-${DOCKER_TAG} )
+        docker manifest annotate --arch $arch "${DOCKER_IMAGE}:${DOCKER_TAG}" $digest
+    done
+
+    docker manifest inspect "${DOCKER_IMAGE}:${DOCKER_TAG}"
+    docker manifest push --purge "${DOCKER_IMAGE}:${DOCKER_TAG}"
+}
+
+[ -n "$SKIP_BUILD" ] && echo "Skipping build." || docker_build
 
 if [ -n "$DRY_RUN" ]; then
     echo "Running in dry run mode. Exiting."
     exit 0
 fi
 
-set +x
-if [ -z "$DOCKER_PASSWORD" ]; then
-    echo "The environment variable DOCKER_PASSWORD needs to be defined"
-    exit 1
-fi
-
-echo "${DOCKER_PASSWORD}" | docker login  --username "${DOCKER_USERNAME}" --password-stdin ${DOCKER_SERVER}
-set -x
-
-digests=""
-for arch in $ARCHES
-do
-    docker push ${DOCKER_IMAGE}:${arch}-${DOCKER_TAG}
-
-    digest=$( docker inspect --format='{{index .RepoDigests 0}}' ${DOCKER_IMAGE}:${arch}-${DOCKER_TAG} )
-    digests="${digests} $digest"
-done
-
-res=0
-for i in {1..6}
-do
-    docker manifest create --amend "${DOCKER_IMAGE}:${DOCKER_TAG}" ${digests} && break || res=$?
-    sleep 10
-done
-[ $res != 0 ] && exit $res
-
-for arch in $ARCHES
-do
-    digest=$( docker inspect --format='{{index .RepoDigests 0}}' ${DOCKER_IMAGE}:${arch}-${DOCKER_TAG} )
-    docker manifest annotate --arch $arch "${DOCKER_IMAGE}:${DOCKER_TAG}" $digest
-done
-
-docker manifest inspect "${DOCKER_IMAGE}:${DOCKER_TAG}"
-docker manifest push --purge "${DOCKER_IMAGE}:${DOCKER_TAG}"
+[ -n "$SKIP_PUSH" ] && echo "Skipping push." || (docker_login; docker_push)
+[ -n "$SKIP_MANIFEST" ] && echo "Skipping manifest." || (docker_login; docker_manifest)
