9229265c8d771d0b507f241cfe723d27f3239f6b
Or Ozeri <oro@il.ibm.com>
2019-01-09 16:40:37 +0200

    add flow subscriber writing to object store

---
+++ contrib/objectstore/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/skydive-project/skydive/common"
+	"github.com/skydive-project/skydive/config"
+	"github.com/skydive-project/skydive/contrib/objectstore/subscriber"
+	shttp "github.com/skydive-project/skydive/http"
+	"github.com/skydive-project/skydive/logging"
+)
+
+const defaultConfigurationFile = "/etc/skydive/skydive-objectstore.yml"
+
+func main() {
+	if err := config.InitConfig("file", []string{defaultConfigurationFile}); err != nil {
+		logging.GetLogger().Errorf("Failed to initialize config: %s", err.Error())
+		os.Exit(1)
+	}
+
+	if err := config.InitLogging(); err != nil {
+		logging.GetLogger().Errorf("Failed to initialize logging system: %s", err.Error())
+		os.Exit(1)
+	}
+
+	cfg := config.GetConfig()
+
+	endpoint := cfg.GetString("endpoint")
+	region := cfg.GetString("region")
+	bucket := cfg.GetString("bucket")
+	accessKey := cfg.GetString("access_key")
+	secretKey := cfg.GetString("secret_key")
+	objectPrefix := cfg.GetString("object_prefix")
+	subscriberURLString := cfg.GetString("subscriber_url")
+	subscriberUsername := cfg.GetString("subscriber_username")
+	subscriberPassword := cfg.GetString("subscriber_password")
+	maxSecondsPerStream := cfg.GetInt("max_seconds_per_stream")
+
+	authOptions := &shttp.AuthenticationOpts{
+		Username: subscriberUsername,
+		Password: subscriberPassword,
+	}
+
+	subscriberURL, err := url.Parse(subscriberURLString)
+	if err != nil {
+		logging.GetLogger().Errorf("Failed to parse subscriber URL: %s", err.Error())
+		os.Exit(1)
+	}
+
+	wsClient, err := config.NewWSClient(common.AnalyzerService, subscriberURL, authOptions, http.Header{})
+	if err != nil {
+		logging.GetLogger().Errorf("Failed to create websocket client: %s", err)
+		os.Exit(1)
+	}
+	structClient := wsClient.UpgradeToStructSpeaker()
+
+	s := subscriber.New(endpoint, region, bucket, accessKey, secretKey, objectPrefix, maxSecondsPerStream)
+
+	// subscribe to the flow updates
+	structClient.AddStructMessageHandler(s, []string{"flow"})
+	structClient.Start()
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+
+	structClient.Stop()
+}
