9cac9fb09e508c7efd673d5187ed198a51423d88
Or Ozeri <oro@il.ibm.com>
2019-03-14 15:28:35 +0200

    do not report new flows

--- contrib/objectstore/subscriber/flowtransformer/custom1/custom1.go
+++ contrib/objectstore/subscriber/flowtransformer/custom1/custom1.go
@@ -1,6 +1,10 @@
 package custom1
 
 import (
+	"time"
+
+	"github.com/pmylund/go-cache"
+
 	"github.com/skydive-project/skydive/flow"
 )
 
@@ -19,10 +23,25 @@ type Flow struct {
 
 // FlowTransformer is a custom transformer for flows
 type FlowTransformer struct {
+	seenFlows *cache.Cache
 }
 
 // Transform transforms a flow before being stored
 func (m *FlowTransformer) Transform(f *flow.Flow) interface{} {
+	// do not report new flows (i.e. the first time you see them)
+	if f.FinishType != flow.FlowFinishType_TIMEOUT {
+		_, seen := m.seenFlows.Get(f.UUID)
+		if f.FinishType == flow.FlowFinishType_NOT_FINISHED {
+			m.seenFlows.Set(f.UUID, true, cache.DefaultExpiration)
+			if !seen {
+				return nil
+			}
+		} else {
+			m.seenFlows.Set(f.UUID, true, time.Minute)
+		}
+	} else {
+		m.seenFlows.Delete(f.UUID)
+	}
 	return &Flow{
 		UUID:             f.UUID,
 		LayersPath:       f.LayersPath,
@@ -36,7 +55,9 @@ func (m *FlowTransformer) Transform(f *flow.Flow) interface{} {
 	}
 }
 
-// New returns a new Custom1Marshaller
+// New returns a new FlowTransformer
 func New() *FlowTransformer {
-	return &FlowTransformer{}
+	return &FlowTransformer{
+		seenFlows: cache.New(10*time.Minute, 10*time.Minute),
+	}
 }
