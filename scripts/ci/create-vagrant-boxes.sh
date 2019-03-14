ec028eb9e2d8fabc0d774f2a01aafb26d732f28f
Sylvain Baubeau <sbaubeau@redhat.com>
2019-03-14 10:00:29 +0100

    ci: increase memory to build dev vagrant boxes

--- scripts/ci/create-vagrant-boxes.sh
+++ scripts/ci/create-vagrant-boxes.sh
@@ -36,7 +36,7 @@ set -e
 for provider in $PROVIDERS
 do
     [ "$provider" = "virtualbox" ] && export PRIVATE_IP=192.168.99.10
-    PREPARE_BOX=true vagrant up --provider=$provider ${KEEP_RESOURCES:+--no-destroy-on-error}
+    MEMORY=8192 PREPARE_BOX=true vagrant up --provider=$provider ${KEEP_RESOURCES:+--no-destroy-on-error}
     [ "$provider" = "libvirt" ] && sudo chmod a+r /var/lib/libvirt/images/dev_dev.img || true
 
     # skydive testing
