b7b9ba1d76509c37b54b1d216385875a558d3d49
Nicolas PLANEL <nplanel@redhat.com>
2019-09-12 10:31:47 +0200

    CI : tags only jenkins registration setup steps

--- scripts/ci/ansible/run-playbook.sh
+++ scripts/ci/ansible/run-playbook.sh
@@ -3,4 +3,4 @@
 
 # install only : jenkins slaves
 dir="$(dirname "$0")"
-ANSIBLE_ROLES_PATH="~/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles:$dir/../../../contrib/ansible/roles" ansible-playbook -i inventory.yml -t slave --skip-tags "vagrant,jenkins" -e ansible_python_interpreter=/usr/bin/python3 -e jenkins_public_hostname=ci.skydive.network -e jenkins_admin_password=password deploy.yml
+ANSIBLE_ROLES_PATH="~/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles:$dir/../../../contrib/ansible/roles" ansible-playbook -i inventory.yml -t slave --skip-tags "vagrant,jenkins_slave_setup" -e ansible_python_interpreter=/usr/bin/python3 -e jenkins_public_hostname=ci.skydive.network -e jenkins_admin_password=password deploy.yml
