d9f5c1a6e3b434a1d7a6a28938903cdca9e23cc6
Nicolas PLANEL <nplanel@redhat.com>
2019-09-27 13:22:17 +0200

    f

--- scripts/ci/run-compile-tests.sh
+++ scripts/ci/run-compile-tests.sh
@@ -8,41 +8,10 @@ dir="$(dirname "$0")"
 set -e
 cd ${GOPATH}/src/github.com/skydive-project/skydive
 
-# prepare collectd build
-rm -rf /tmp/collectd
-git clone https://github.com/collectd/collectd.git /tmp/collectd
 
-# Compile all contribs
-COLLECTD_SRC=/tmp/collectd make contribs
+sudo cat /etc/ssh/sshd_config
+sudo perl -i -pe 's|MINIO_.*||g' /etc/ssh/sshd_config
 
-# Compile with default options
-make
-make test.functionals.compile TAGS=${TAGS}
-
-export CGO_CFLAGS="-I/usr/local/include/dpdk -O3 -g -std=gnu11 -m64 -pthread -march=native -DRTE_MACHINE_CPUFLAG_SSE -DRTE_MACHINE_CPUFLAG_SSE2 -DRTE_MACHINE_CPUFLAG_SSE3 -DRTE_MACHINE_CPUFLAG_SSSE3 -DRTE_MACHINE_CPUFLAG_SSE4_1 -DRTE_MACHINE_CPUFLAG_SSE4_2 -DRTE_MACHINE_CPUFLAG_PCLMULQDQ -DRTE_MACHINE_CPUFLAG_RDRAND -DRTE_MACHINE_CPUFLAG_FSGSBASE -DRTE_MACHINE_CPUFLAG_F16C -include rte_config.h -Wno-deprecated-declarations"
-export CGO_LDFLAGS="-L/usr/local/lib"
-
-# Compile with all build options supported enabled
-make WITH_DPDK=true WITH_EBPF=true WITH_VPP=true WITH_EBPF_DOCKER_BUILDER=true WITH_K8S=true WITH_ISTIO=true \
-    WITH_HELM=true VERBOSE=true
-
-# Compile Skydive for Windows
-GO111MODULE=on GOOS=windows GOARCH=amd64 go build github.com/skydive-project/skydive
-
-# Compile Skydive for MacOS
-GO111MODULE=on GOOS=darwin GOARCH=amd64 go build github.com/skydive-project/skydive
-
-# Compile profiling
-make WITH_PROF=true VERBOSE=true
-
-make clean
-
-# Compile all tests
-make test.functionals.compile TAGS=${TAGS} WITH_NEUTRON=true WITH_SELENIUM=true WITH_CDD=true \
-    WITH_SCALE=true WITH_EBPF=true WITH_EBPF_DOCKER_BUILDER=true WITH_VPP=true WITH_K8S=true \
-    WITH_ISTIO=true WITH_HELM=true
-
-make clean
-
-# Compile static
-make static
+echo ==============
+sudo cat /etc/ssh/sshd_config
+sudo systemctl restart sshd
