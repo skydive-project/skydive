be7d88413fd7cb6a5d099d0c8c7a449c00ecfda9
Sylvain Afchain <safchain@gmail.com>
2019-09-27 10:35:12 +0200

    cdd: report scale logs in case of error

--- scripts/ci/run-cdd-overview-tests.sh
+++ scripts/ci/run-cdd-overview-tests.sh
@@ -8,7 +8,6 @@ sudo iptables -F
 sudo iptables -P FORWARD ACCEPT
 for i in $(find /proc/sys/net/bridge/ -type f) ; do echo 0 | sudo tee $i ; done
 
-set -e
 cd ${GOPATH}/src/github.com/skydive-project/skydive
 make install
 
@@ -17,6 +16,15 @@ export SKYDIVE=${GOPATH}/bin/skydive
 export SKYDIVE_LOGGING_LEVEL=DEBUG
 
 make test.functionals WITH_CDD=true TAGS=${TAGS} VERBOSE=true TIMEOUT=10m TEST_PATTERN=Overview
+status=$?
+
+cat /tmp/skydive-scale/{analyzer,agent}-?.log | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | perl -ne '$d=$1 if /^(\d+-\d+-\d+),/; $k{$d}.=$_; END{print $k{$_} for sort keys(%k);}'
+if [ $status -ne 0 ] ; then
+   echo "test Scale TLS:${TLS} FLOW_PROTOCOL:${FLOW_PROTOCOL} failed return ${status}"
+   exit $status
+fi
+
+set -e
 
 "${dir}/convert-to-gif.sh" tests/overview.mp4 tests/overview-tmp.gif
 gifsicle -O3 tests/overview-tmp.gif -o tests/overview.gif
