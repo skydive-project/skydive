1561a979f7b64309652140e9a967dfee59f77705
Sylvain Afchain <safchain@gmail.com>
2019-10-07 15:08:37 +0200

    ui: fix metadata key order

--- statics/js/components/object-detail.js
+++ statics/js/components/object-detail.js
@@ -35,57 +35,70 @@ Vue.component('object-detail', {
 
   template: '\
     <div class="object-detail">\
-      <div class="object-key-value" v-for="(value, key) in object" :class="[typeof(key) == \'string\' ? key.toLowerCase() : key]">\
-        <div v-if="Array.isArray(value)">\
-          <collapse :collapsed="collapsedState(path(key))">\
+      <div class="object-key-value" v-for="obj in orderedObject" :class="[typeof(obj.key) == \'string\' ? obj.key.toLowerCase() : obj.key]">\
+        <div v-if="Array.isArray(obj.value)">\
+          <collapse :collapsed="collapsedState(path(obj.key))">\
             <div slot="collapse-header" slot-scope="props" class="object-key">\
-            {{key}} :\
+            {{obj.key}} :\
             <span class="pull-right">\
               <i class="glyphicon glyphicon-chevron-left rotate" :class="{\'down\': props.active}"></i>\
             </span>\
             </div>\
             <div slot="collapse-body">\
-              <div v-for="(v, index) in value">\
+              <div v-for="(v, index) in obj.value">\
                 <div v-if="typeof v == \'object\'" class="object-sub-detail" style="margin-left: 20px;">\
                   <span v-if="Object.keys(v).length > 0" class="object-key" :class="typeof(value)" style="float:left">- </span>\
-                  <object-detail :object="v" :pathPrefix="path(key)" :transformer="transformer" :collapsed="collapsed"></object-detail>\
+                  <object-detail :object="v" :pathPrefix="path(obj.key)" :transformer="transformer" :collapsed="collapsed"></object-detail>\
                 </div>\
                 <div v-else class="object-sub-detail">\
-                  <div class="object-detail copy-clipboard" :class="typeof(value)" @click="copyToClipboard(v)">- {{ transform(key, v) }}</div>\
+                  <div class="object-detail copy-clipboard" :class="typeof(obj.value)" @click="copyToClipboard(v)">- {{ transform(obj.key, v) }}</div>\
                 </div>\
               </div>\
             </div>\
           </collapse>\
         </div>\
-        <div v-else-if="typeof value == \'object\' && !$.isEmptyObject(value)" class="object-sub-detail">\
-          <collapse :collapsed="collapsedState(path(key))">\
+        <div v-else-if="typeof obj.value == \'object\' && !$.isEmptyObject(obj.value)" class="object-sub-detail">\
+          <collapse :collapsed="collapsedState(path(obj.key))">\
             <div slot="collapse-header" slot-scope="props" class="object-key">\
-              {{key}} :\
+              {{obj.key}} :\
               <span class="pull-right">\
                 <i class="glyphicon glyphicon-chevron-left rotate" :class="{\'down\': props.active}"></i>\
               </span>\
             </div>\
             <div slot="collapse-body">\
-              <object-detail :object="value" :pathPrefix="path(key)" :transformer="transformer" :collapsed="collapsed"></object-detail>\
+              <object-detail :object="obj.value" :pathPrefix="path(obj.key)" :transformer="transformer" :collapsed="collapsed"></object-detail>\
             </div>\
           </collapse>\
         </div>\
-        <div v-else-if="typeof value == \'boolean\'">\
-          <span class="object-key">{{key}}</span> :\
+        <div v-else-if="typeof obj.value == \'boolean\'">\
+          <span class="object-key">{{obj.key}}</span> :\
           <span class="object-value copy-clipboard">\
             <i v-if="value == true" class="fa fa-check bool-value-true" aria-hidden="true"></i>\
             <i v-else class="fa fa-times bool-value-false" aria-hidden="true"></i>\
           </span>\
         </div>\
         <div v-else>\
-          <span class="object-key">{{key}}</span> :\
-          <span v-if="typeof value != \'object\' || !$.isEmptyObject(value)" class="object-value copy-clipboard" :class="typeof(value)" v-html="transform(key, value)" @click="copyToClipboard(value)"></span>\
-          <i v-if="links && links[key]" :class="links[key].class" @click="links[key].onClick" \
-            @mouseover="links[key].onMouseOver" @mouseout="links[key].onMouseOut"></i>\
+          <span class="object-key">{{obj.key}}</span> :\
+          <span v-if="typeof obj.value != \'object\' || !$.isEmptyObject(obj.value)" class="object-value copy-clipboard" :class="typeof(obj.value)" v-html="transform(obj.key, obj.value)" @click="copyToClipboard(obj.value)"></span>\
+          <i v-if="links && links[obj.key]" :class="links[obj.key].class" @click="links[obj.key].onClick" \
+            @mouseover="links[obj.key].onMouseOver" @mouseout="links[obj.key].onMouseOut"></i>\
         </div>\
       </div>\
     </div>\
   ',
+
+  computed: {
+    orderedObject: function() {
+      var entries = [];
+
+      for (var key in this.object) {
+        entries.push({"key": key, "value": this.object[key]});
+      }
+
+      entries.sort(function(a, b) { return a.key.localeCompare(b.key)});
+      return entries;
+    }
+  },
 
   methods: {
 
