8e155f10e18c646fd0a8f6e0860992ac6696e99d
Adrian Moreno <amorenoz@redhat.com>
2021-06-02 12:34:19 +0200

    ovnmodel: check-in generated files
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- topology/probes/ovn/ovnmodel/types.go
+++ topology/probes/ovn/ovnmodel/types.go
@@ -2,8 +2,9 @@ package ovnmodel
 
 // OVNLink describes a Link between two OVN tables
 type OVNLink struct {
-	SourceTable string // source table name
-	SourceField string // source field name
-	DestTable   string // destination table name
-	DestField   string // destination field table name
+	SourceTable  string // source table name
+	SourceField  string // source field name
+	DestTable    string // destination table name
+	DestField    string // destination field table name
+	Relationship string // relationship type
 }
